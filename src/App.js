05749a978d532a8eeb91d4160886f5e587e58aba
volodymyr-shvachko <vshvac@softserveinc.com>
2021-08-19 19:09:16 +0300

    feat: sign in logic

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Home, SignIn, SignUp } from './pages';
 
-export const App = memo(() => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/sign_in" component={SignIn} />
-      <Route exact path="/sign_up" component={SignUp} />
-    </Switch>
-  );
-});
+export const App = memo(() => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/sign_in" component={SignIn} />
+    <Route exact path="/sign_up" component={SignUp} />
+  </Switch>
+));
